3df582260b82018d0a0ad29a5ea2892067c1766a
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2015-01-17 16:46:09 +0100

    - removed second deklaration of max_coverage attribute in Sam::Seq initialization

--- lib/Sam/Seq.pm
+++ lib/Sam/Seq.pm
@@ -747,7 +747,6 @@ sub new{
 		bin_size => $BinSize,
 		bin_max_bases => $BinSize * $MaxCoverage,
 		phred_offset => $PhredOffset,
-		max_coverage => 50,
 		is => undef,
 		## custom overwrites
 		@_,
