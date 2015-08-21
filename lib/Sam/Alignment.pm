b8062d925d5c7c916042cf0add87ce38a9f8bc51
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2015-08-21 15:36:57 +0200

    added GAP constant and add gap parameter to ->seq_states() which defaults to GAP

--- lib/Sam/Alignment.pm
+++ lib/Sam/Alignment.pm
@@ -7,7 +7,11 @@ use overload
     'bool' => sub{1},
     '""' => \&string;
 
-our $VERSION = '1.1.3';
+use constant {
+    GAP => '', # or '-'
+};
+
+our $VERSION = '1.1.4';
 
 =head1 NAME
 
@@ -461,14 +465,17 @@ sub seq_aligned{
 
 =head2 seq_states
 
-Get the states of a sequence based on cigar.
+Get the states of a sequence based on cigar. By default gaps are represented by
+char defined with GAP constant (''), can be overwritten with (gap => '-');
 
 =cut
 
 sub seq_states{
-    my ($self) = @_;
+    my $self = shift;
 
     return undef if $self->seq eq "*";
+
+    my %p = (gap => GAP, @_);
 
     my $cigar = $self->cigar;
     my $seq = $self->seq;
@@ -483,7 +490,7 @@ sub seq_states{
             $s[-1].= substr($seq, $pos, $1) if @s;
             $pos+=$1;
         } elsif ($2 eq "D") {
-            push @s, ('-') x $1;
+            push @s, ($p{gap}) x $1;
         } else {
             push @s, split(//, substr($seq, $pos, $1));
             $pos+=$1;
